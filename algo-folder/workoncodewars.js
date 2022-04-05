bcd2ea3b00c1501d6cef889fc2813b9c58112d9a
Chloe <chloe84@msn.com>
2022-04-04 22:59:18 -0400

    work on codewars hw

---
+++ algo-folder/workoncodewars.js
@@ -0,0 +1,29 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+// For example (Input --> Output):
+
+function persistence(num) {
+    //   turned number into a string 
+      num += ''
+      let sum = 1
+    //   split string after each index
+      let numStr = num.split('')
+    //   console.log(numStr)
+    //   for loop through the string 
+    //   for(let i = 1; i > numStr.length; i++){
+    //     if(numStr[i] >= 10)
+    //       numStr[i] * 
+    //   }
+      
+     
+    }
+
+    // We want to create a function that will add numbers together when called in succession.
+    // We also want to be able to continue to add numbers to our chain.
+    // A single call should be equal to the number passed in.
+    // We should be able to store the returned values and reuse them.
+    // We can assume any number being passed in will be valid whole number.
+    // add(1)(2); // 
+
+    function add(n) {
+    }
+  
